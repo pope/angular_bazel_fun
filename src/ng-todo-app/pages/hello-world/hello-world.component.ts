63b8a66e6bc1ab9ef2c9691a6d4c5561798a07a7
K. Adam Christensen <pope@shifteleven.com>
2020-03-01 09:18:02 -0800

    Formatting

--- src/ng-todo-app/pages/hello-world/hello-world.component.ts
+++ src/ng-todo-app/pages/hello-world/hello-world.component.ts
@@ -1,9 +1,9 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'hello-world',
   templateUrl: 'hello-world.component.html',
-  styleUrls: ['./hello-world.component.scss']
+  styleUrls: ['./hello-world.component.scss'],
 })
 export class HelloWorldComponent {
   name: string = 'Adam';
--- src/ng-todo-app/pages/hello-world/hello-world.module.ts
+++ src/ng-todo-app/pages/hello-world/hello-world.module.ts
@@ -1,15 +1,19 @@
-import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import {HelloWorldComponent} from './hello-world.component';
+import { HelloWorldComponent } from './hello-world.component';
 
 @NgModule({
   declarations: [HelloWorldComponent],
   imports: [
     RouterModule,
-    RouterModule.forChild([{path: '', component: HelloWorldComponent}])
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HelloWorldComponent,
+      },
+    ]),
   ],
   exports: [HelloWorldComponent],
 })
-export class HelloWorldModule {
-}
+export class HelloWorldModule {}
