3e63cadb68ef8a1d742d2ff00502cec8150884f3
K. Adam Christensen <pope@google.com>
2020-03-04 22:50:06 +0000

    Add some more styles

--- src/ng-todo-app/app/app.component.ts
+++ src/ng-todo-app/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'todo-app',
+  styleUrls: ['./app.component.scss'],
   templateUrl: 'app.component.html',
 })
 export class AppComponent {}
